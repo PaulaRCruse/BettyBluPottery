30f1f95d08db9d9b7953da3af11afa956afb2808
Paula Cruse <PaulaCruse@hotmail.com>
2020-11-14 17:07:11 -0500

    updating code 2

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ window.onclick = function(event) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns(i);
+            var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
